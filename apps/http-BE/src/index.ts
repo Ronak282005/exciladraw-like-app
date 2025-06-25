16c4fe1b9334430a230beeb419ad4cda330be4d4
ronak <ronakpraja340@gmail.com>
2025-06-26 01:24:53 +0530

    signup route

--- apps/http-BE/src/index.ts
+++ apps/http-BE/src/index.ts
@@ -1,11 +1,23 @@
 import express from "express";
+import { z } from "zod";
+import bcrypt from "bcrypt";
 
 const app = express();
 
+const bodyInput = z.object({
+  username: z.string(),
+  password: z.string(),
+});
 
-app.post("/signup", (req, res) => {
-    const {username,password} = req.body
-    
+app.post("/signup", async (req, res) => {
+  const { success } = bodyInput.safeParse(req.body);
+  const { password, username } = req.body;
+  if (!success) {
+    res.status(403).json({
+      msg: "invalid inputs!",
+    });
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
 });
 
 app.post("/signin", (req, res) => {});
