ef3d1362649efdd328051e49f9cbc2349cb24b73
ronak <ronakpraja340@gmail.com>
2025-06-26 00:42:28 +0530

    middleware

--- apps/http-BE/src/index.ts
+++ apps/http-BE/src/index.ts
@@ -1,19 +1,13 @@
-import express from "express"
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.post("/signup",(req,res)=>{
+app.post("/signup", (req, res) => {});
 
-})
+app.post("/signin", (req, res) => {});
 
-app.post("/signin",(req,res)=>{
+app.post("/create-room", (req, res) => {});
 
-})
-
-app.post("/create-room",(req,res)=>{
-    
-})
-
-app.listen(3001,()=>{
-    console.log("listening on the port 3000");
-})
+app.listen(3001, () => {
+  console.log("listening on the port 3000");
+});
