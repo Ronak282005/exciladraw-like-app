5e73e75b62cc81c6731c99d7385670b1a231b791
ronak <ronakpraja340@gmail.com>
2025-06-26 00:33:59 +0530

    defined the endpoints

--- apps/http-BE/src/index.ts
+++ apps/http-BE/src/index.ts
@@ -2,6 +2,17 @@ import express from "express"
 
 const app = express()
 
+app.post("/signup",(req,res)=>{
+
+})
+
+app.post("/signin",(req,res)=>{
+
+})
+
+app.post("/create-room",(req,res)=>{
+    
+})
 
 app.listen(3001,()=>{
     console.log("listening on the port 3000");
