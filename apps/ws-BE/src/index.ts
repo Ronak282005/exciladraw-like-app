acab8e8851b3fb4bec0c13cf4dca7d0610980288
ronak <ronakpraja340@gmail.com>
2025-06-26 01:53:00 +0530

    ws-parms

--- apps/ws-BE/src/index.ts
+++ apps/ws-BE/src/index.ts
@@ -1,15 +1,15 @@
 import { WebSocketServer } from "ws";
 
-const wss = new WebSocketServer({port : 8080})
+const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection",(ws,request)=>{
-    const url = request.url
-    if(!url){
-        return
-    }
-    const queryParm = new URLSearchParams(url.split("?")[1])
-    const token = queryParm.get("token")
-    ws.on("message",(data)=>{
-        ws.send("pong")
-    })
-})
+wss.on("connection", (ws, request) => {
+  const url = request.url;
+  if (!url) {
+    return;
+  }
+  const queryParm = new URLSearchParams(url.split("?")[1]);
+  const token = queryParm.get("token");
+  ws.on("message", (data) => {
+    ws.send("pong");
+  });
+});
