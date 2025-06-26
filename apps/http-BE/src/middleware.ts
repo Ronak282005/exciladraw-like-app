06153479bc77520125dc3732d00cc378cd0f163e
ronak <ronakpraja340@gmail.com>
2025-06-26 15:22:53 +0530

    dotenv config

--- apps/http-BE/src/middleware.ts
+++ apps/http-BE/src/middleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
-import { ENV } from "./config";
+// import { ENV } from "./config";
+import {ENV} from "@repo/backend-common"
 
 export const authMiddleware = (
   req: Request,
