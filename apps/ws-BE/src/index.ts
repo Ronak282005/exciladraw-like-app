9703fa80fb5ab79cc048791e1ffc1f6b0b74efa5
ronak <ronakpraja340@gmail.com>
2025-06-26 12:57:55 +0530

    jwt verify and ws

--- apps/ws-BE/src/index.ts
+++ apps/ws-BE/src/index.ts
@@ -1,5 +1,5 @@
 import { WebSocketServer } from "ws";
-import jwt, { JwtPayload } from "jsonwebtoken"
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 const wss = new WebSocketServer({ port: 8080 });
 
@@ -10,10 +10,10 @@ wss.on("connection", (ws, request) => {
   }
   const queryParm = new URLSearchParams(url.split("?")[1]);
   const token = queryParm.get("token");
-  const decode = jwt.verify(token,JWT_SECRET)
-  if(!decode || !(decode as JwtPayload).userId){
-    ws.close()
-    return
+  const decode = jwt.verify(token, JWT_SECRET);
+  if (!decode || !(decode as JwtPayload).userId) {
+    ws.close();
+    return;
   }
   ws.on("message", (data) => {
     ws.send("pong");
