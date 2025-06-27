6bd2543739daaf2dc03ddde238f86447d1b41680
ronak <ronakpraja340@gmail.com>
2025-06-27 12:32:53 +0530

    room logic in ws-be and message

--- apps/ws-BE/src/index.ts
+++ apps/ws-BE/src/index.ts
@@ -1,7 +1,7 @@
 import { WebSocket, WebSocketServer } from "ws";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { ENV } from "@repo/backend-common/config";
-
+import {prismaClient} from "@repo/db/client"
 const wss = new WebSocketServer({ port: 8080 });
 
 interface User {
@@ -40,7 +40,46 @@ wss.on("connection", (ws, request) => {
     ws,
     rooms: [],
   });
-  ws.on("message", (data) => {
-    ws.send("pong");
-  });
+ws.on("message", async (data) => {
+  if (typeof data !== "string") {
+    return null;
+  }
+  const parsedData = JSON.parse(data);
+  if (parsedData.type === "join_room") {
+    const user = users.find(x => x.ws === ws);
+    user?.rooms.push(parsedData.roomId);
+  }
+
+  if (parsedData.type === "leave_room") {
+    const user = users.find(x => x.ws === ws);
+    if (!user) {
+      return;
+    }
+    user.rooms = user.rooms.filter(x => x === parsedData.room);
+  }
+
+  if (parsedData.type === "chat") {
+    const roomId = parsedData.roomId;
+    const message = parsedData.message;
+
+    await prismaClient.chat.create({
+      data: {
+        roomId,
+        message,
+        userId
+      }
+    });
+
+    users.forEach(user => {
+      if (user.rooms.includes(roomId)) {
+        user.ws.send(JSON.stringify({
+          type: "chat",
+          message: message,
+          roomId
+        }));
+      }
+    });
+  
+}
+  })
 });
