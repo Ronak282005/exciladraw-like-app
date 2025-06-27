f66a9f35498756b77aa46782d7a1196d82b7b1fe
ronak <ronakpraja340@gmail.com>
2025-06-27 11:52:54 +0530

    room logic in ws-be

--- apps/ws-BE/src/index.ts
+++ apps/ws-BE/src/index.ts
@@ -4,17 +4,28 @@ import {ENV} from "@repo/backend-common/config"
 
 const wss = new WebSocketServer({ port: 8080 });
 
+function checkUser(token : string) : string | null {
+  const decode = jwt.verify(token as string, ENV.JWT_SECRET);
+  if( typeof decode == "string"){
+    return null;
+  }
+  if (!decode || !decode.userId) {
+    return null;
+  }
+  return decode.userId
+}
+
 wss.on("connection", (ws, request) => {
   const url = request.url;
   if (!url) {
     return;
   }
   const queryParm = new URLSearchParams(url.split("?")[1]);
-  const token = queryParm.get("token");
-  const decode = jwt.verify(token as string, ENV.JWT_SECRET);
-  if (!decode || !(decode as JwtPayload).userId) {
-    ws.close();
-    return;
+  const token = queryParm.get("token") || "";
+  const userId = checkUser(token)
+  if (userId == null) {
+    ws.close()
+    return
   }
   ws.on("message", (data) => {
     ws.send("pong");
